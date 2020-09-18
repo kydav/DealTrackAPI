2a6999abf66732d34e4a4421a0a99d9f44472cef
Kyler Davis <kyler.davis@maritzcx.com>
2020-09-17 21:58:37 -0600

    EF Core

---
+++ DealTrackAPI/Models/DealTrackDBContext.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+
+namespace DealTrackAPI.Models
+{
+    public class DealTrackDBContext : DbContext
+    {
+        //https://www.entityframeworktutorial.net/efcore/create-model-for-existing-database-in-ef-core.aspx
+        public DealTrackDBContext()
+        {
+        }
+    }
+}
